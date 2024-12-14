7954c683185bb53296aec98e1d7c68d7cfd9c7b4
CiaL-eXstaSe <j.l.vietinghoff@gmail.com>
2024-12-14 10:53:12 +0100

    Added read functionality to main.py and create call for main()

--- main.py
+++ main.py
@@ -1,1 +1,14 @@
-print("hello world")
+# TODO:
+# - Use a main function to wrap the logic and call main() at the bottom of your file 
+#   to execute your program.
+# - Change main.py so that instead of printing "hello world", it reads the contents 
+#   of the "frankenstein.txt" and prints it all to the console.
+
+def main():
+    with open("books/frankenstein.txt") as f:
+        file_contents = f.read()
+        print(file_contents)
+
+if __name__ == "__main__":
+    main()
+
